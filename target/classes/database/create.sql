770930acbdbe1e9e3a4d8aeaa5b6e4aa981fb6fd
nhatbarry <nhatbarry@gmail.com>
2024-12-29 03:19:09 +0700

    upd db

--- target/classes/database/create.sql
+++ target/classes/database/create.sql
@@ -48,18 +48,15 @@ CREATE TABLE DatPhong (
     maHomestay INT,
 	maNguoiDung INT,
     ngayNhanPhong DATE,
-    trangThai NVARCHAR(50),
     FOREIGN KEY (maPhong) REFERENCES Phong(maPhong),
 	FOREIGN KEY (maHomestay) REFERENCES Homestay(maHomestay),
 	FOREIGN KEY (maNguoiDung) REFERENCES nguoidung(manguoidung)
 );
 go
-Alter VIEW timphong
+create VIEW timphong
 as
 select 
-	h.tenHomestay, p.maPhong, p.loaiPhong, p.giaPhong, h.diaChi, p.trangThai
+	h.maHomestay, h.tenHomestay, p.maPhong, p.loaiPhong, p.giaPhong, h.diaChi, p.trangThai
 from phong p
 join homestay h on p.maHomestay = h.maHomestay
 
-select * from timphong
-select * from homestay where diaChi like N'%Hà Nội%'
--- target/classes/database/insert.sql
+++ target/classes/database/insert.sql
@@ -39,34 +39,4 @@ INSERT INTO Phong (loaiPhong, giaPhong, trangThai, maHomestay) VALUES
 ('Suite', 200.0, 'Available', 8),
 ('Single', 100.0, 'Available', 9);
 go
-INSERT INTO ThanhToan (maNguoiDung, maPhong, ngayThanhToan, soTien) VALUES
-(1, 1, '2024-01-01', 100.0),
-(2, 2, '2024-01-02', 150.0),
-(3, 3, '2024-01-03', 200.0),
-(4, 4, '2024-01-04', 100.0),
-(5, 5, '2024-01-05', 150.0),
-(6, 6, '2024-01-06', 200.0),
-(7, 7, '2024-01-07', 100.0),
-(8, 8, '2024-01-08', 150.0),
-(9, 9, '2024-01-09', 200.0),
-(10, 10, '2024-01-10', 100.0);
-go
-INSERT INTO DatPhong (maPhong, ngayNhanPhong, trangThai) VALUES
-(1, '2024-01-11', 'Confirmed'),
-(2, '2024-01-12', 'Confirmed'),
-(3, '2024-01-13', 'Pending'),
-(4, '2024-01-14', 'Confirmed'),
-(5, '2024-01-15', 'Cancelled'),
-(6, '2024-01-16', 'Confirmed'),
-(7, '2024-01-17', 'Pending'),
-(8, '2024-01-18', 'Confirmed'),
-(9, '2024-01-19', 'Cancelled'),
-(10, '2024-01-20', 'Confirmed');
-go
 
-delete NguoiDung
-delete homestay
-delete phong
-DBCC CHECKIDENT ('[NguoiDung]', RESEED, 0);
-DBCC CHECKIDENT ('[homestay]', RESEED, 0);
-DBCC CHECKIDENT ('[phong]', RESEED, 0);
